35224d241fad112f3f81a7690a4f811aa57963b2
Felipe Lira dos santos <santosfelipeliipe@gmail.com>
2023-11-11 17:00:31 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express")
 const app = express()
 
+app.get("/", (req,res)=>{
+    res.send("olá mundo")
+})
+
+app.listen(3000, ()=>{
+    console.log("servidor está rodando na porta 3000!")
+})
+
