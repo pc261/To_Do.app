25b36bbaf73b6ad3390a97fe1f0018b9395022c4
Felipe Lira dos santos <santosfelipeliipe@gmail.com>
2023-11-20 20:52:44 -0300

    iniciando a conexão com o mysql

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express")
 const app = express()
+const mysql = require("mysql2")
 
 app.get("/", (req,res)=>{
     res.render('home')
 })
 
-app.listen(3000, ()=>{
-    console.log("servidor está rodando na porta 3000!")
-})
+
 
 // --Configurando o Handlebars 
 // -Para baixar o Handlebars: npm i express-handlebars
@@ -18,6 +17,15 @@ app.set('view engine', 'handlebars')
 // --Definindo uma pasta de arquivos estáticos 
 app.use(express.static('public'))
 
+// criando uma conexão com o MYSQL:
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"root",
+    database:"todo.app",
+    // Caso o banco de dados esteja rodando na porta 3307 é necessario mudar a port a baixo para port: 3307
+    port: 3306
+})
 
 
 
