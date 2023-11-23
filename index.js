db076b5450d11e1d0e5aae9e0611b5089353eba2
Felipe Lira dos santos <santosfelipeliipe@gmail.com>
2023-11-23 20:15:10 -0300

    salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const conexao = mysql.createConnection({
 
 conexao.connect((erro)=>{
     if (erro){
-        console.log(erro)
+        return console.log(erro)
     } 
     
     console.log("conectado ao mysql")
@@ -38,3 +38,31 @@ conexao.connect((erro)=>{
         console.log("Servidor rodando na porta 3000")
     })
 })
+
+// Convertendo a informação do HTML para que o js entenda
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+// ROTAS
+app.post("/criar", (req,res)=>{
+    const descricao= req.body.descricao 
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro)=>{
+        if (erro){
+            return console.log(erro)
+        }
+
+        res.redirect('/')
+    })
+})
+
